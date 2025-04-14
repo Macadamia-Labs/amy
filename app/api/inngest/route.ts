91b8d4fff8cb5d495a2de9f0d6e514e802fd29c5
bgpierre <brecht.pierreux@gmail.com>
2025-04-14 15:43:13 -0700

    decrease to 5 minutes

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -4,8 +4,8 @@ import { deepSearchInngest as deepSearch } from '@/lib/inngest/deep-search'
 import { processFile } from '@/lib/inngest/process-file'
 import { serve } from 'inngest/next'
 
-// Set timeout to 50 minutes (3000 seconds)
-export const maxDuration = 3000
+// Set timeout to 5 minutes (300 seconds)
+export const maxDuration = 300
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
